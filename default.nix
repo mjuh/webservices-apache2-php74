6aee11f88059062190d78c3c4da9f03f8e0ef872
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-19 19:05:50 +0300

    default.nix: Use ref argument.

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{}:
+{ ref ? "master" }:
 
 with import <nixpkgs> {
   overlays = [
-    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = (if builtins ? getEnv then builtins.getEnv "GIT_BRANCH" else "master"); }))
+    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; inherit ref; }))
   ];
 };
 
