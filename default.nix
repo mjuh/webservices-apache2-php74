bd297b4562cadb15c41788544322f196597daa52
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-21 19:18:19 +0300

    Revert "add some ci labels"
    This reverts commit 940e0ada90dbc45ff89c74bf339429e5d426dcfe.

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ with import <nixpkgs> {
 };
 
 let
-  inherit (builtins) concatMap getEnv replaceStrings toJSON;
+  inherit (builtins) concatMap getEnv toJSON;
   inherit (dockerTools) buildLayeredImage;
   inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd mkRootfs;
   inherit (lib.attrsets) collect isDerivation;
@@ -27,13 +27,6 @@ let
     mimeTypes = mime-types;
     libstdcxx = gcc-unwrapped.lib;
   };
-
-gitAbbrev = firstNChars 8 (getEnv "GIT_COMMIT");
-gitCommit = (getEnv "GIT_COMMIT");
-jenkinsBuildUrl = (getEnv "BUILD_URL");
-jenkinsJobName = (getEnv "JOB_NAME");
-jenkinsBranchName = (getEnv "BRANCH_NAME");
-gitlabCommitUrl = "https://gitlab.intr/" + (replaceStrings [jenkinsBranchName ""] ["" ""] jenkinsJobName) + "/commit/" + gitCommit;
 
 in
 
@@ -73,8 +66,6 @@ pkgs.dockerTools.buildLayeredImage rec {
       ru.majordomo.docker.arg-hints-json = builtins.toJSON php74DockerArgHints;
       ru.majordomo.docker.cmd = dockerRunCmd php74DockerArgHints "${name}:${tag}";
       ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
-      ru.majordomo.ci.jenkins.build.url = if jenkinsBuildUrl != "" then jenkinsBuildUrl + "console" else "none";
-      ru.majordomo.ci.gitlab.commit.url = if gitlabCommitUrl != "" then gitlabCommitUrl else "none";
     };
     extraCommands = ''
       set -xe
