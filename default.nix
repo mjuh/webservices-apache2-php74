175658a0136ab541919bc13b20b0ca14fff849bc
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-17 10:56:05 +0300

    Add imagemagick to PATH

--- default.nix
+++ default.nix
@@ -44,6 +44,7 @@ pkgs.dockerTools.buildLayeredImage rec {
     libjpeg_turbo
     jpegoptim
     (optipng.override{ inherit libpng ;})
+    imagemagick
     gifsicle nss-certs.unbundled zip
     gcc-unwrapped.lib
     glibc
