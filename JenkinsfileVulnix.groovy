f35fd74ce891b16c5adbdfd8f03936746f2fa485
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-12 14:46:28 +0300

    JenkinsfileVulnix.groovy: Add whitelist.

--- JenkinsfileVulnix.groovy
+++ JenkinsfileVulnix.groovy
@@ -18,7 +18,7 @@ pipeline {
                 script {
                     sh "mkdir -p $JUNIT_OUTPUT_DIRECTORY $VULNIX_CACHE_DIRECTORY"
                     writeFile (file: JUNIT_OUTPUT_JSON,
-                               text: (sh (script: "vulnix --cache-dir $VULNIX_CACHE_DIRECTORY --json $params.DOCKER_IMAGE || true",
+                               text: (sh (script: "vulnix --whitelist https://gitlab.intr/webservices/vulnix/-/raw/master/whitelist.toml --cache-dir $VULNIX_CACHE_DIRECTORY --json $params.DOCKER_IMAGE || true",
                                           returnStdout: true)).trim())
                     sh "scripts/vulnix2junit.py"
                     junit JUNIT_OUTPUT_XML
